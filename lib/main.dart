df3b0694df4263beaff5cadfd307e7e93eaaaaa4
yohom <382146139@qq.com>
2018-11-03 15:24:52 +0800

    生成的json字符串复制到粘贴板; 增加文字颜色

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:json_to_dart/model_generator.dart';
+import 'package:clippy/server.dart' as clippy;
+import 'package:colorize/colorize.dart';
 
-void main(List<String> args) {
+void main(List<String> args) async {
   final className = args[0];
   final jsonString = args[1];
 
   final classGenerator = ModelGenerator(className);
   String dartClassString = classGenerator.generateDartClasses(jsonString);
+
+  await clippy.write(dartClassString);
   print(dartClassString);
+  print(Colorize('The json string has been copied to the paste board ~')
+    ..yellow());
 }
